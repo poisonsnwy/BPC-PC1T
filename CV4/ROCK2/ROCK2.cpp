ec7f25cf2b0526ddd28f54473a3527a4dab34c53
poisonsnwy <poisonsnwy@gmail.com>
2025-10-09 10:25:09 +0200

    123

---
+++ CV4/ROCK2/ROCK2.cpp
@@ -0,0 +1,36 @@
+﻿// ROCK2.cpp : Defines the entry point for the application.
+//
+
+#include "ROCK2.h"
+
+using namespace std;
+
+
+int jePrestupny(int rok) {
+
+	if (rok % 4 == 0 && rok % 100 != 0 || rok % 400 == 0) {
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+
+}
+
+
+int main() {
+	int rok;
+	int rokz;
+	int rokk;
+	printf("Zadejte prvni rok:\n");
+	scanf_s("%d", &rokz);
+	printf("Zadejte prvni rok:\n");
+	scanf_s("%d", &rokk);
+
+	for (int i = rokz; i <= rokk; i++) {
+		int result = jePrestupny(i);
+		printf("Rok %d je prestupny: %d\n", i, result);
+	}
+	return 0;
+}
