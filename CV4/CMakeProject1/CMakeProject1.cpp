593470352d7b86d7c07449f0477f8f7145d43fd8
poisonsnwy <poisonsnwy@gmail.com>
2025-10-16 10:20:55 +0200

    1123123

---
+++ CV4/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,26 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProject1.h"
+
+using namespace std;
+
+
+int jelichy(int rok) {
+	if (rok % 2 != 0)
+		return 1;
+	}
+
+int main() {
+
+
+	int cislo;
+	printf("zadajte cislo ktore chcete urcit");
+	scanf("%d", &cislo);
+
+
+	printf("%d\t%d", cislo, jelichy(cislo));
+
+	return 0;
+
+}
