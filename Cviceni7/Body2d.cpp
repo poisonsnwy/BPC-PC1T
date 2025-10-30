8b874f14c342c4b02f10cec87708088d883ed7a4
poisonsnwy <97610019+poisonsnwy@users.noreply.github.com>
2025-10-30 12:30:35 +0100

    Add files via upload

---
+++ Cviceni7/Body2d.cpp
@@ -0,0 +1,45 @@
+
+
+#include "Body2d.h"
+#include <math.h>
+#include <stdio.h>
+
+int indexNejblizsi(int velikost, int body[][2], int referencniX, int referencniY)
+{
+    int nejblizsiIndex = 0;
+    double nejmensiVzdalenost = sqrt(
+        pow(body[0][0] - referencniX, 2) +
+        pow(body[0][1] - referencniY, 2)
+    );
+
+    for (int i = 1; i < velikost; i++) {
+        double vzdalenost = sqrt(
+            pow(body[i][0] - referencniX, 2) +
+            pow(body[i][1] - referencniY, 2)
+        );
+
+        if (vzdalenost < nejmensiVzdalenost) {
+            nejmensiVzdalenost = vzdalenost;
+            nejblizsiIndex = i;
+        }
+    }
+
+    return nejblizsiIndex;
+}
+
+double prumernaVzdalenost(int velikost, int body[][2], int referencniX, int referencniY) {
+    double Delka =0.0;
+    for (int i = 0; i < velikost; i++) {
+        
+        double localDelka = sqrt(
+            pow(body[i][0] - referencniX, 2) +
+            pow(body[i][1] - referencniY, 2)
+        );
+        Delka += localDelka;
+    }
+
+    double prumer = Delka / velikost;
+    return prumer;
+}
+
+   
