06a3661ffbd2b27344e1f6bb52d2e021c388e672
poisonsnwy <97610019+poisonsnwy@users.noreply.github.com>
2025-10-30 12:29:04 +0100

    Delete Cviceni7 directory

--- Cviceni7/Body2d.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-int indexNejblizsi(int velikost, int (*poleBodu)[2], int referencniX, int
-referencniY);
-double prumernaVzdalenost(int velikost, int (*poleBodu)[2], int referencniX, int
-referencniY);
