212ad0f7b504f80e015d521a42d6c890c323424b
Student <Student@PC-SE5115-03>
2025-11-06 10:44:27 +0100

    r

--- Cviceni8/Kiro.cpp
+++ Cviceni8/Kiro.cpp
@@ -8,11 +8,32 @@ using namespace std;
 #pragma once
 int prunik(double zacatek1, double konec1, double zacatek2, double konec2, double* vysl_zacatek, double* vysl_konec)
 {
-	if (zacatek1 <= konec2 && zacatek2 <= konec1) {
-		*vysl_zacatek = max(zacatek1, zacatek2);
-		*vysl_konec = min(konec1, konec2);
-		return 1;
-	}
-	return 0;
+    {
+        if (zacatek1 > konec1) {
+            double t = zacatek1;
+            zacatek1 = konec1;
+            konec1 = t;
+        }
+        if (zacatek2 > konec2) {
+            double t = zacatek2;
+            zacatek2 = konec2;
+            konec2 = t;
+        }
+
+        if (zacatek1 > zacatek2) {
+            double tz = zacatek1, tk = konec1;
+            zacatek1 = zacatek2;
+            konec1 = konec2;
+            zacatek2 = tz;
+            konec2 = tk;
+        }
+
+        if (zacatek1 <= konec2 && zacatek2 <= konec1) {
+            *vysl_zacatek = max(zacatek1, zacatek2);
+            *vysl_konec = min(konec1, konec2);
+            return 1;
+        }
+        return 0;
+    }
 }
 
