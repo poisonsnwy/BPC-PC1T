c738176e8186d1f12431fed45c0d3611367f8b36
Student <Student@PC-SE5115-03>
2025-11-13 10:34:52 +0100

    cv9/1

---
+++ cv9/cv9/VectorModul.cpp
@@ -0,0 +1,41 @@
+﻿#include "VectorModul.h"
+#include <stdio.h>
+
+
+
+struct vector3d operace(struct vector3d u, struct vector3d v, enum typOperace typ) {
+	struct vector3d r;
+
+	switch (typ)
+	{
+	case soucet:
+		r.x = u.x + v.x;
+		r.y = u.y + v.y;
+		r.z = u.z + v.z;
+
+		break;
+	case vektorovySoucin:
+
+		r.x = u.y * v.z - u.z * v.y;
+		r.y = u.z * v.x - u.x * v.z;
+		r.z = u.x * v.y - u.y * v.x;
+		break;
+	}
+	return r;
+
+}
+void tisk(struct vector3d r, enum typOperace typ) {
+	switch (typ) {
+	case soucet:
+		printf("Součet vektoru: ");
+		break;
+	case vektorovySoucin:
+		printf("Vektorový součin: ");
+		break;
+	default:
+		printf("Neznámá operace: ");
+		break;
+	}
+	printf("(x=%.2f, y=%.2f, z=%.2f)\n", r.x, r.y, r.z);
+
+}
---
+++ cv9/cv9/cv9.cpp
@@ -0,0 +1,35 @@
+﻿// cv9.cpp : Defines the entry point for the application.
+//
+
+#include "cv9.h"
+#include "VectorModul.h"
+
+using namespace std;
+
+int main()
+{
+	
+	printf("If you want to get a summ press 1, if you want to get a vector product press 2.");
+	int Pass;
+	scanf_s("%d",&Pass);
+
+
+	printf("Write down data of the first vector (a b c)");
+	int x1, y1, z1;
+	scanf_s("%d" "%d" "%d", &x1, &y1, &z1);
+	struct vector3d u = { x1, y1, z1 };
+
+	printf("Write down data of the second vector (a b c)");
+	int x2, y2, z2;
+	scanf_s("%d" "%d" "%d", &x2, &y2, &z2);
+	struct vector3d v = { x2, y2, z2 };
+
+
+	struct vector3d r = { 0,0,0 };
+	r = operace(u, v, (enum typOperace)Pass);
+	 
+
+	tisk(r, (enum typOperace)Pass);
+
+	return 0;
+}
