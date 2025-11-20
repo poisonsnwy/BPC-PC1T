6e0fdd7bb15ef6d42a28b135c42fd9703e138282
Student <Student@PC-SE5115-03>
2025-11-20 10:26:55 +0100

    CV10 has been added

---
+++ cv10/cv10/cv10.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE* fvstup = fopen("D:/283144/rep/cv10/cv10/vstup.txt", "r");
+    int letters = 0;
+    int digits = 0;
+    int ch;
+    char hledany;
+    printf("Zadejte libovolny znak: ");
+    scanf("%c", &hledany);
+    int pocet = 0;
+
+    while ((ch = fgetc(fvstup)) != EOF) {
+        if (isalpha(ch)) letters++;
+        if (isdigit(ch)) digits++;
+        if (ch == hledany) pocet++;
+    }
+    fclose(fvstup);
+    FILE* fvys = fopen("D:/283144/rep/cv10/cv10/vysledek.txt", "w");
+    fprintf(fvys, "Letters: %d\nDigits: %d\nHledany znak: %d\n", letters, digits, pocet);
+    fclose(fvys);
+    printf("Letters: %d\nDigits: %d\nHledany znak: %d\n", letters, digits, pocet);
+    return 0;
+}
